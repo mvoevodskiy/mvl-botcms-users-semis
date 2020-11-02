d4ba8c1bd395c546a9f8755bdbac532f64732cca
mvoevodskiy <mv@aaa0.ru>
2020-11-02 07:40:49 +0300

    Added associations to member model

--- src/model/botcmschatmember.js
+++ src/model/botcmschatmember.js
@@ -1,8 +1,21 @@
 module.exports = (Sequelize) => {
-  return {
-    joined: Sequelize.DATE,
-    permissions: Sequelize.STRING,
-    state: Sequelize.STRING,
-    banUntil: Sequelize.DATE
-  }
+  return [
+    {
+      joined: Sequelize.DATE,
+      permissions: Sequelize.STRING,
+      state: Sequelize.STRING,
+      banUntil: Sequelize.DATE
+    },
+    {},
+    {
+      belongsTo: [
+        {
+          model: 'mvlBotCMSChat'
+        },
+        {
+          model: 'mvlBotCMSUser'
+        }
+      ]
+    }
+  ]
 }
